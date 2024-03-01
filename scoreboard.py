9fbd644cf9d0f5d873ef8cc85de23fa2ca8198af
Stella329 <stella33299@gmail.com>
2024-03-01 18:11:35 +0800

    new submit

---
+++ scoreboard.py
@@ -0,0 +1,40 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        with open('data.txt') as data:
+            self.highest_score = int(data.read())
+
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.write(f"Score: {self.score}   Highest_Score: {self.highest_score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def highest(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+            with open('data.txt', mode="w") as data:
+                data.write(str(self.highest_score))
+
+        self.score = 0 ## note the indentation!!
+        self.clear()
+        self.update()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update()
+
+    def update(self):
+        self.write(f"Score: {self.score}   Highest_Score: {self.highest_score}", align=ALIGNMENT, font=FONT)
